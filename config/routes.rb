97fbdf6a68d81bacfecf58e8646869f386d79b30
Shodai-Takehara <higo.hh.bb.moccos@gmail.com>
2021-10-09 00:07:55 +0900

    Add: sorcery login画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'top_page#top'
+
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
+  resources :users, only: %i[new create]
 end
