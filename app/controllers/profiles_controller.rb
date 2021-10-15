f200516c5d67056d8d7068dd1387d5118234feaa
Shodai-Takehara <higo.hh.bb.moccos@gmail.com>
2021-10-15 23:10:49 +0900

    Add: profile編集画面

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,27 @@
+class ProfilesController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to profile_path, success: '更新が完了しました'
+    else
+      flash.now['danger'] = '更新に失敗しました'
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,7 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
-  validates :email, uniqueness: true
-  validates :email, presence: true
+  validates :email, uniqueness: true, presence: true
 
   validates :name, presence: true, length: { maximum: 255 }
 
