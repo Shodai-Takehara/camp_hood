b2ff79bd158e58948801d4ce060287752d39e62d
Shodai-Takehara <higo.hh.bb.moccos@gmail.com>
2021-11-23 01:23:33 +0900

    Add: キャンプ場詳細ページ作成・Campsiteテーブルにimageカラム追加

--- app/controllers/campsites_controller.rb
+++ app/controllers/campsites_controller.rb
@@ -1,12 +1,13 @@
 class CampsitesController < ApplicationController
-  skip_before_action :require_login, only: %i[index]
+  skip_before_action :require_login, only: %i[index show]
   before_action :set_campsite, only: %i[show]
 
   def index
     @campsites = Campsite.all.page(params[:page])
   end
 
-  def show ; end
+  def show
+  end
 
   private
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_145125) do
+ActiveRecord::Schema.define(version: 2021_11_22_161342) do
 
   create_table "authentications", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_11_18_145125) do
     t.text "contact_link"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
