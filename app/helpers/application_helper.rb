d7094c0a5e8313754b71287648b54e68f3cf37e4
Shodai-Takehara <higo.hh.bb.moccos@gmail.com>
2021-11-25 21:36:52 +0900

    Add: URLのViewヘルパー作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,20 @@
 module ApplicationHelper
+  require "uri"
 
   def page_title(page_title = '')
     base_title = 'CAMPHOOD'
     page_title.empty? ? base_title : page_title + ' | ' + base_title
+  end
+
+  # URLテキストにリンクを付与する
+  def text_url_to_link(text)
+    URI.extract(text, ['http','https'] ).uniq.each do |url|
+      sub_text = ""
+      sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+
+      text.gsub!(url, sub_text)
+    end
+    text
   end
 
   # def campsite_count
