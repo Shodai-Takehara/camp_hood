0ae9340afeeb791e57b5bb58cab2a6e379a0842e
Shodai-Takehara <higo.hh.bb.moccos@gmail.com>
2021-12-13 12:47:01 +0900

    Add: assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,7 @@
 # Be sure to restart your server when you modify this file.
+Rails.application.config.assets.configure do |env|
+  env.export_concurrent = false
+end
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
