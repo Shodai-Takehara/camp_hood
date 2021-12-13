b648e013cb66bf64d111313f11135927ac14fd9a
Shodai-Takehara <higo.hh.bb.moccos@gmail.com>
2021-12-13 11:26:54 +0900

    Remove: admin-lte

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -1,4 +1,10 @@
-import "admin-lte/plugins/bootstrap/js/bootstrap.bundle.min";
-import "admin-lte/dist/js/adminlte";
-import "jquery";
+// import "admin-lte/plugins/bootstrap/js/bootstrap.bundle.min";
+// import "admin-lte/dist/js/adminlte";
+require("jquery");
 require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+
+import "bootstrap";
+import "../src/application";
