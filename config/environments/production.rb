393a29f56622a2cd5451db3155b15bb82f5eb4dd
Shodai-Takehara <higo.hh.bb.moccos@gmail.com>
2021-12-29 16:47:09 +0900

    Add: config/settings/production.yml追記

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # 本番環境のメール設定
+  config.action_mailer.default_url_options = Settings.default_url_options.to_h
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
